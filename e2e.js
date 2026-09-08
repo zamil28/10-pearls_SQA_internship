3c55b6befc8eba8b870ec3b19a712c879ce0594a
zamil28 <zamilme143@gmail.com>
2026-09-08 20:42:21 +0500

    Delete e2e.js

--- e2e.js
+++
@@ -1,9 +0,0 @@
-// This file runs before every single spec file.
-// It's the ideal place to load global custom commands (Task 5).
-import "./commands";
-
-// Example of a global hook you could extend later, e.g. to suppress
-// uncaught exceptions thrown by the app under test that aren't relevant
-// to the assertions being made:
-//
-// Cypress.on("uncaught:exception", () => false);
